508b2132224664ac4b2f61fc271d2884934e3e5b
Student406-11 <Student406-11@NPK101O-3>
2024-10-08 12:59:24 +0700

    asdasds

--- QRGenerator_winforms/Form1.Designer.cs
+++ QRGenerator_winforms/Form1.Designer.cs
@@ -28,18 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            LinkTextBox = new RichTextBox();
+            GenerateButton = new Button();
+            QRPictureBox = new PictureBox();
+            ClearButton = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)QRPictureBox).BeginInit();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(ClearButton);
+            panel1.Controls.Add(LinkTextBox);
+            panel1.Controls.Add(GenerateButton);
+            panel1.Dock = DockStyle.Right;
+            panel1.Location = new Point(803, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(239, 611);
+            panel1.TabIndex = 0;
+            // 
+            // LinkTextBox
+            // 
+            LinkTextBox.Dock = DockStyle.Top;
+            LinkTextBox.Location = new Point(0, 0);
+            LinkTextBox.Name = "LinkTextBox";
+            LinkTextBox.Size = new Size(239, 96);
+            LinkTextBox.TabIndex = 2;
+            LinkTextBox.Text = "";
+            // 
+            // GenerateButton
+            // 
+            GenerateButton.Dock = DockStyle.Bottom;
+            GenerateButton.Location = new Point(0, 553);
+            GenerateButton.Name = "GenerateButton";
+            GenerateButton.Size = new Size(239, 58);
+            GenerateButton.TabIndex = 0;
+            GenerateButton.Text = "Сгенерировать";
+            GenerateButton.UseVisualStyleBackColor = true;
+            GenerateButton.Click += GenerateButton_Click;
+            // 
+            // QRPictureBox
+            // 
+            QRPictureBox.Dock = DockStyle.Fill;
+            QRPictureBox.Location = new Point(0, 0);
+            QRPictureBox.Name = "QRPictureBox";
+            QRPictureBox.Size = new Size(803, 611);
+            QRPictureBox.TabIndex = 1;
+            QRPictureBox.TabStop = false;
+            // 
+            // ClearButton
+            // 
+            ClearButton.Dock = DockStyle.Bottom;
+            ClearButton.Location = new Point(0, 495);
+            ClearButton.Name = "ClearButton";
+            ClearButton.Size = new Size(239, 58);
+            ClearButton.TabIndex = 3;
+            ClearButton.Text = "Очистить";
+            ClearButton.UseVisualStyleBackColor = true;
+            ClearButton.Click += ClearButton_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1042, 611);
+            Controls.Add(QRPictureBox);
+            Controls.Add(panel1);
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)QRPictureBox).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button GenerateButton;
+        private PictureBox QRPictureBox;
+        private RichTextBox LinkTextBox;
+        private Button ClearButton;
     }
 }
--- QRGenerator_winforms/Form1.cs
+++ QRGenerator_winforms/Form1.cs
@@ -1,3 +1,5 @@
+using QRCoder;
+
 namespace QRGenerator_winforms
 {
     public partial class Form1 : Form
@@ -5,6 +7,42 @@ namespace QRGenerator_winforms
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(LinkTextBox.Text) || string.IsNullOrWhiteSpace(LinkTextBox.Text) || LinkTextBox.Text == null || LinkTextBox.Text == "")
+            {
+                try
+                {
+                    QRCodeGenerator qrGenerator = new QRCodeGenerator();
+
+                    QRCodeData data = qrGenerator.CreateQrCode(LinkTextBox.Text.Trim(), QRCodeGenerator.ECCLevel.Q);
+
+                    QRCode qrCode = new QRCode(data);
+
+                    Bitmap qrCodeImage = qrCode.GetGraphic(5);
+
+                    QRPictureBox.Image = new Bitmap(qrCodeImage, QRPictureBox.Width, QRPictureBox.Height);
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка генерации QR-кода: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Поле ввода не должно быть пустым.", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            LinkTextBox.Clear();
+            QRPictureBox.Image = null;
         }
     }
 }
